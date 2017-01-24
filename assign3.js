cc6ef73413583f82e895c0553ce6e21d374aea02
gurjeet95 <gv9810@gmail.com>
2017-01-24 21:56:10 +0000

    updated code

--- assign3.js
+++ assign3.js
@@ -1,19 +1,15 @@
 const values=process.argv
-console.log(values[2])
 let num1=Number(values[2])
 let num2=Number(values[3])
 let sum=num1+num2
-if(sum>100 && sum<1000)
-{
-    console.log(`sum is greater than 100 but less than 1000`)
-}
-else if(sum<100)
-{
- console.log(`sum is less than 100`)   
-    
-}
-else if(sum>10)
+let output
+if(sum>10 && sum<100)
 {
     
-    console.log(`sum is greater than 10`)
+    output="sum is greater than 10 but less than 100"
 }
+else if(sum>100 && sum<1000)
+{
+    output="sum is greater than 100 but less than 1000"
+}
+console.log(output)
--- assign4.js
+++ assign4.js
@@ -1,5 +1,8 @@
 const values=process.argv
-let mealcost=Number(values[2])
-let tip=Number(values[3])
-let totalmealcost=mealcost+((tip/100)*mealcost)
-console.log(`Total meal cost is ${totalmealcost}`)
+const mealCost = Number(values[2])
+const tipPercent = Number(values[3])
+const tipAmount = (tipPercent / 100) * mealCost
+const totalOwing = tipAmount + mealCost
+const outPut = `your meal was $${mealCost} + a ${tipPercent}% tip = ${totalOwing}`
+
+console.log(outPut)
